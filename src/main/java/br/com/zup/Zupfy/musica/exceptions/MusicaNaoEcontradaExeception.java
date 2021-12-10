9711856b5a396cfadf8c756046e18fff7d7b71cc
Raquel Leão <vc.raquel@gmail.com>
2021-12-10 14:38:54 -0300

    adicionar atributo de mensagem na MusicaNaoEncontradaException

--- src/main/java/br/com/zup/Zupfy/musica/exceptions/MusicaNaoEcontradaExeception.java
+++ src/main/java/br/com/zup/Zupfy/musica/exceptions/MusicaNaoEcontradaExeception.java
@@ -1,4 +1,8 @@
 package br.com.zup.Zupfy.musica.exceptions;
 
 public class MusicaNaoEcontradaExeception extends RuntimeException{
+
+    public MusicaNaoEcontradaExeception(String message) {
+        super(message);
+    }
 }
