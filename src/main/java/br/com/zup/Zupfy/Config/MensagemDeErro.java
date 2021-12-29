4ce861c9616a6e8add37c2e27ed40c69940f86ac
Raquel Leão <vc.raquel@gmail.com>
2021-12-29 20:24:48 -0300

    criar classe MensagemDeErro para manipular exceptions

--- src/main/java/br/com/zup/Zupfy/Config/MensagemDeErro.java
+++ src/main/java/br/com/zup/Zupfy/Config/MensagemDeErro.java
@@ -1,2 +1,17 @@
-package br.com.zup.Zupfy.Config;public class MmensagemDeErro {
+package br.com.zup.Zupfy.Config;
+
+public class MensagemDeErro {
+    private String mensagem;
+
+    public MensagemDeErro(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
 }
